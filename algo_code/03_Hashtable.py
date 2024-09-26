5929ecee68f997845d40ca52fc10ecdc32c733e0
Code <56292332+liu-longfei@users.noreply.github.com>
2024-09-26 09:26:58 +0800

    Update 03_Hashtable.py

--- algo_code/03_Hashtable.py
+++ algo_code/03_Hashtable.py
@@ -1,3 +1,4 @@
+# 242.有效的字母异位词
 class Solution_2:
     @classmethod
     def isAnagram(cls, s: str, t: str):
@@ -17,6 +18,7 @@ t = 'bacdz'
 print(Solution_2().isAnagram(s, t))
 
 
+# 349. 两个数组的交集
 class Solution_3:
     @classmethod
     def intersection(cls, nums1: list[int], nums2: list[int]):
@@ -34,3 +36,29 @@ class Solution_3:
 nums1 = [1, 2, 3, 4]
 nums2 = [2, 4, 6, 7]
 print(Solution_3.intersection(nums1, nums2))
+
+
+# 四、第202题. 快乐数
+class Solution_4:
+    def get_sum(self, n):
+        new_sum = 0
+        while n:
+            n, r = divmod(n, 10)
+            new_sum += r * r
+        return new_sum
+
+    @classmethod
+    def isHappy(cls, n):
+        record = set()
+        while True:
+            new_sum = cls.get_sum(cls, n)
+            # print(new_sum)
+            if new_sum == 1:
+                return True
+            elif new_sum in record:
+                return False
+            else:
+                record.add(new_sum)
+                n = new_sum
+
+print(Solution_4.isHappy(129))
