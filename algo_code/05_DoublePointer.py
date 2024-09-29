3863c85c6b0b95a681f413df312eff7516db8552
Code <56292332+liu-longfei@users.noreply.github.com>
2024-09-29 09:46:09 +0800

    Update 05_DoublePointer.py

--- algo_code/05_DoublePointer.py
+++ algo_code/05_DoublePointer.py
@@ -11,3 +11,18 @@ class Solution_1:
 
 list_1 = [1, 2, 2, 3, 3, 4, 5]
 print(Solution_1.removeElement(list_1, 1))
+
+
+class Solution_2:
+    @classmethod
+    def reverseStr(cls, s: list[str]):
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return s
+
+
+list_1 = ['a', 'b', 'c', 'd']
+print(Solution_2.reverseStr(list_1))
