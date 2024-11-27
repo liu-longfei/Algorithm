17aaf8eb3e62c785e5c841bac99e34b0c54423f7
Code <56292332+liu-longfei@users.noreply.github.com>
2024-11-27 09:43:09 +0800

    Update 08_BacktrackingAlgorithm.py

--- algo_code/08_BacktrackingAlgorithm.py
+++ algo_code/08_BacktrackingAlgorithm.py
@@ -17,3 +17,25 @@ class Solution_2:
 
 
 print(Solution_2.combine(5, 3))
+
+
+# 三、77.组合优化
+class Solution_3:
+    @classmethod
+    def combine(cls, n, k):
+        result = []
+        cls.backtracking(n, k, 1, [], result)
+        return result
+
+    @classmethod
+    def backtracking(cls, n, k, start_index, path, result):
+        if len(path) == k:
+            result.append(path[:])
+
+        for i in range(start_index, n - (k - len(path)) + 2):
+            path.append(i)
+            cls.backtracking(n, k, i, path, result)
+            path.pop()
+
+
+print(Solution_2.combine(5, 3))
