fb1f3d68f1ee99fa4645443d6675fa2d6e173315
Code <56292332+liu-longfei@users.noreply.github.com>
2024-12-02 09:23:27 +0800

    Update 10_DynamicProgramming.py

--- algo_code/10_DynamicProgramming.py
+++ algo_code/10_DynamicProgramming.py
@@ -21,3 +21,20 @@ class Solution_2:
 
 print(Solution_2.fib(10))
 print(Solution_2.fib_2(10))
+
+
+# 三、70. 爬楼梯
+class Solution_3:
+    @classmethod
+    def climb_stair(cls, n: int):
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+
+print(Solution_3.climb_stair(3))
