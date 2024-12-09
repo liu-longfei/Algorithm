690b3fdf1bb2f6be3602bc716924c3216199fbde
Code <56292332+liu-longfei@users.noreply.github.com>
2024-12-09 10:12:57 +0800

    Update 12_GraphTheory.py

--- algo_code/12_GraphTheory.py
+++ algo_code/12_GraphTheory.py
@@ -32,5 +32,47 @@ def main_3():
             print(' '.join(map(str, i)))
 
 
+# 99. 岛屿数量, 深搜版
+class Solution_5:
+    direction = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    @classmethod
+    def dfs(cls, grid, visited, x, y):
+        for i, j in cls.direction:
+            next_x = x + i
+            next_y = y + j
+            if next_x < 0 or next_x >= len(grid) or next_y < 0 or next_y >= len(grid[0]):
+                continue
+            if not visited[next_x][next_y] and grid[next_x][next_y]:
+                visited[next_x][next_y] = True
+                cls.dfs(grid, visited, next_x, next_y)
+
+    @classmethod
+    def main(cls):
+        '''
+4 5
+1 1 0 0 0
+1 1 0 0 0
+0 0 1 0 0
+0 0 0 1 1
+        '''
+        n, m = map(int, input().split())
+
+        grid = []
+        for i in range(n):
+            grid.append(list(map(int, input().split())))
+
+        visited = [[False] * m for _ in range(n)]
+
+        res = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] and not visited[i][j]:
+                    visited[i][j] = True
+                    res += 1
+                    cls.dfs(grid, visited, i, j)
+        return res
+
+
 if __name__ == '__main__':
-    main_3()
+    # main_3()
+    print(Solution_5.main())
