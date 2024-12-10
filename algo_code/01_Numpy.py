2d6725261ef4911318b63546b3214007879a9867
Code <56292332+liu-longfei@users.noreply.github.com>
2024-12-10 09:25:59 +0800

    Update 01_Numpy.py

--- algo_code/01_Numpy.py
+++ algo_code/01_Numpy.py
@@ -86,8 +86,48 @@ class Solution_5:
                 min_len = min_len if min_len < len_ else len_
                 sum -= nums[idx]
                 idx += 1
-            
+
         return 0 if min_len == float('inf') else min_len
 
 
 print(Solution_5.min_subarray_length(7, [2, 3, 1, 2, 4, 3]))
+
+
+# 59.螺旋矩阵II
+class Solution_6:
+    @classmethod
+    def generate_matrix(cls, n: int):
+        nums = [[0] * n for _ in range(n)]
+        startx, starty = 0, 0
+        loop = n // 2
+        mid = n // 2
+        count = 1
+
+        for offset in range(1, loop + 1):
+
+            for i in range(starty, n - offset):
+                nums[startx][i] = count
+                count += 1
+
+            for i in range(startx, n - offset):
+                nums[i][n - offset] = count
+                count += 1
+
+            for i in range(n - offset, starty, -1):
+                nums[n - offset][i] = count
+                count += 1
+
+            for i in range(n - offset, startx, -1):
+                nums[i][starty] = count
+                count += 1
+
+            startx += 1
+            starty += 1
+
+        if n % 2:
+            nums[mid][mid] = count
+        return nums
+
+
+for i in Solution_6.generate_matrix(7):
+    print(i)
