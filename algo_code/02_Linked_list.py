3c89b17a741abf3763125decd0f5c420964a7a1c
Code <56292332+liu-longfei@users.noreply.github.com>
2024-12-11 09:56:47 +0800

    Update 02_Linked_list.py

--- algo_code/02_Linked_list.py
+++ algo_code/02_Linked_list.py
@@ -55,7 +55,38 @@ class Solution_4:
         while cur:
             tmp = cur.next
             cur.next = pre
-            
+
             pre = cur
             cur = tmp
         return pre
+
+
+# 五、24. 两两交换链表中的节点
+class Solution_5:
+    @classmethod
+    def swap_paires(cls, head: ListNode):
+        dummy_node = ListNode(next=head)
+        current_node = dummy_node
+
+        while current_node.next and current_node.next.next:
+            temp = current_node.next
+            temp1 = current_node.next.next.next
+
+            current_node.next = current_node.next.next
+            current_node.next.next = temp
+            current_node.next.next.next = temp1
+
+            current_node = current_node.next.next
+        return dummy_node.next
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+
+curr = Solution_5.swap_paires(head)
+print('----------------------------------')
+while curr:
+    print(curr.val, end=" -> ")
+    curr = curr.next
