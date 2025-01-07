04799f654d8aec0595d1e3de77b19795a2c5e5dd
Code <56292332+liu-longfei@users.noreply.github.com>
2025-01-07 08:46:06 +0800

    Update 12_GraphTheory.py

--- algo_code/12_GraphTheory.py
+++ algo_code/12_GraphTheory.py
@@ -73,6 +73,45 @@ class Solution_5:
         return res
 
 
-if __name__ == '__main__':
-    # main_3()
-    print(Solution_5.main())
+
+# main_3()
+# print(Solution_5.main())
+
+
+# 六、99. 岛屿数量，广搜版
+class Solution_6:
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    @classmethod
+    def bfs(cls, grid, visited, i, j):
+        from collections import deque
+        que = deque([])
+        que.append([i, j])
+        while que:
+            cur_x, cur_y = que.popleft()
+            for x, y in cls.directions:
+                next_x = cur_x + x
+                next_y = cur_y + y
+                if next_y < 0 or next_x < 0 or next_x >= len(grid) or next_y >= len(grid[0]):
+                    continue
+                if grid[next_x][next_y] and not visited[next_x][next_y]:
+                    visited[next_x][next_y] = True
+                    que.append([next_x, next_y])
+
+    @classmethod
+    def main(cls):
+        n, m = map(int, input().split())
+
+        grid = []
+        for i in range(n):
+            grid.append(list(map(int, input().split())))
+        visited = [[False] * m for _ in range(n)]
+        res = 0
+        for i in range(n):
+            for j in range(m):
+                if not visited[i][j] and grid[i][j]:
+                    res += 1
+                    cls.bfs(grid, visited, i, j)
+        print(res)
+
+
+Solution_6.main()
